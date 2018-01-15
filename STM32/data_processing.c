34a5e8e2e908e707b0fc6cff04963987f1d01606
Dzvinka Yarish <yarish@ucu.edu.ua>
2018-01-15 06:41:44 +0200

    added comments

--- STM32/data_processing.c
+++ STM32/data_processing.c
@@ -1,6 +1,8 @@
 #include "data_processing.h"
 
-int processData(Array data){
+//count how many real strikes were made based on distances data
+int processData(Array data) {
+	//states for the system
 	int TDS = 0;
 	int BDS = 1;
 	int UP_M = 2;
@@ -14,69 +16,60 @@ int processData(Array data){
 
 	states[0][0] = cur_state;
 	states[0][1] = prev_dist;
-
-	printf("Data size: %d", data.used);
-
-	for(int i = 1; i < data.used; i++){
+	for(int i = 1; i < data.used; i++) {
 //		printf("%d\n", count);
-		if(((i != 1 || i != data.used - 1)||(i != 2 || i != data.used - 2)) && ((data.array[i] < data.array[i+1]) && (data.array[i] > data.array[i-1])) && ((data.array[i + 1] <= data.array[i+2]) && (data.array[i - 1] >= data.array[i-2]))){
-			printf(" first %d %d %d %d %d \n", data.array[i - 2],data.array[i - 1], data.array[i],data.array[i + 1], data.array[i + 2]);
-			continue;
-				}
-		else if(((i != 1 || i != data.used - 1)||(i != 2 || i != data.used - 2)) && ((data.array[i] > data.array[i+1]) && (data.array[i] < data.array[i-1])) && ((data.array[i + 1] >= data.array[i+2]) && (data.array[i -1] <= data.array[i-2]))){
-			printf("second %d %d %d %d %d\n", data.array[i - 2],data.array[i - 1], data.array[i],data.array[i + 1], data.array[i + 2]);
-
-			continue;
-		}
-		else if (cur_state == TDS){
+//		if(((i != 1 || i != data.used - 1)||(i != 2 || i != data.used - 2)) && ((data.array[i] < data.array[i+1]) && (data.array[i] > data.array[i-1])) && ((data.array[i + 1] =< data.array[i+2]) && (data.array[i - 1] >= data.array[i-2]))){
+//			printf(" first %d %d %d %d %d \n", data.array[i - 2],data.array[i - 1], data.array[i],data.array[i + 1], data.array[i + 2]);
+//			continue;
+//				}
+//		else if(((i != 1 || i != data.used - 1)||(i != 2 || i != data.used - 2)) && ((data.array[i] > data.array[i+1]) && (data.array[i] < data.array[i-1])) && ((data.array[i + 1] > data.array[i+2]) && (data.array[i -1] =< data.array[i-2]))){
+//			printf("second %d %d %d %d %d\n", data.array[i - 2],data.array[i - 1], data.array[i],data.array[i + 1], data.array[i + 2]);
+//
+//			continue;
+//		}
+		if (cur_state == TDS) {
 			if (prev_dist > data.array[i]) {
 				cur_state = DOWN_M;
 			}
 		}
-		else if(cur_state == BDS){
+		else if(cur_state == BDS) {
 			if (prev_dist < data.array[i]){
 				cur_state = UP_M;
 			}
 		}
-		else if (cur_state == UP_M){
-            if (prev_dist == data.array[i]){
+		else if (cur_state == UP_M) {
+            if (prev_dist == data.array[i]) {
             	cur_state = TDS;
             	states[count][0] = cur_state;
             	states[count][1] = prev_dist;
             	count += 1;
             }
-            else if(prev_dist > data.array[i]){
+            else if(prev_dist > data.array[i]) {
             	states[count][0] = TDS;
             	states[count][1] = prev_dist;
-//            	printf("TDS = %d\n", states[count][0]);
-//            	printf("Distance = %d\n", states[count][1]);
             	cur_state = DOWN_M;
             	count += 1;
             }
 		}
-		else if(cur_state == DOWN_M){
-			if (prev_dist == data.array[i]){
+		else if(cur_state == DOWN_M) {
+			if (prev_dist == data.array[i]) {
 				cur_state = BDS;
 				states[count][0] = cur_state;
 				states[count][1] = prev_dist;
 				count += 1;
-			} else if(prev_dist < data.array[i]){
+			} else if(prev_dist < data.array[i]) {
 				states[count][0] = BDS;
 				states[count][1] = prev_dist;
-//				printf("BDS = %d\n", states[count][0]);
-//				printf("Distance = %d\n", states[count][1]);
 				cur_state = UP_M;
 				count += 1;
 			}
 		}
 
-	prev_dist = data.array[i];
-
-
+		prev_dist = data.array[i];
 	}
 	printf("COUNT: %d\n", count);
 	int dlength = 0;
-	for(int c = 0; c < count-1; c+=2){
+	for(int c = 0; c < count-1; c+=2) {
 //    		deltas[countd] = abs(states[c][1] - states[c + 1][1]);
 //    		printf("%d\n", deltas[countd]);
 		dlength+=1;
@@ -87,7 +80,8 @@ int processData(Array data){
 	int deltas[dlength];
 
 	int countd = 0;
-	for(int c = 0; c < count-1; c+=2){
+  //calculate delta for each pair TDS - BDS
+	for(int c = 0; c < count-1; c+=2) {
 		deltas[countd] = abs(states[c][1] - states[c + 1][1]);
 //		printf("%d\n", deltas[countd]);
 		countd+=1;
@@ -95,17 +89,18 @@ int processData(Array data){
 
 	int max = -1000;
 
-	for(int d = 0; d < countd; d+=1){
+	for(int d = 0; d < countd; d+=1) {
 		if(deltas[d] > max){
 			max = deltas[d];
 		}
 	}
 	printf("MAX: %d\n", max);
 
+  //find the biggest number of similar deltas - the number of real strikes
 	int number = 0;
-	for(int e = 0; e < countd; e+=1){
+	for(int e = 0; e < countd; e+=1) {
 		printf("DELTA: %d\n", deltas[e]);
-		if((deltas[e]>=max-5)&&(deltas[e]<=max+5)){
+		if((deltas[e] >= max-5)&&(deltas[e] <= max+5)){
 			number ++;
 		}
 	}
@@ -115,7 +110,6 @@ int processData(Array data){
 
 void sendToAndroid(int numbStrikes) {
 	printf("Strikes: %d\n", numbStrikes);
-	//	int lst[1] = {2000};
 	char temp[100] = "";
 	sprintf(temp, "%u;", numbStrikes);
 	USARTSendString(temp);
